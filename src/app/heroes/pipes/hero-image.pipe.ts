4aba89b4f300c6f045a60f7aa4cca652191cecca
Campuzano90 <carlos.javier.giles@hotmail.com>
2023-08-16 18:08:59 -0600

    Continuacion del CRUD - seccion 15

--- src/app/heroes/pipes/hero-image.pipe.ts
+++ src/app/heroes/pipes/hero-image.pipe.ts
@@ -7,7 +7,7 @@ import { Hero } from '../interfaces/heroe.interface';
 export class HeroImagePipe implements PipeTransform {
   transform(hero: Hero): string {
     if (!hero.id && !hero.alt_img) {
-      return 'assets/noimage.png';
+      return 'assets/no-image.png';
     }
 
     if (hero.alt_img) return hero.alt_img;
